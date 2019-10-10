146f464d1cd92028c83d88451523e45d68474319
wangjianyong <wangjianyong@orvibo.com>
2019-10-10 18:30:29 +0800

    添加播放xib

---
+++ ergediandian/vc/PlayViewController.h
@@ -0,0 +1,17 @@
+//
+//  PlayViewController.h
+//  ergediandian
+//
+//  Created by peanut on 2019/10/10.
+//  Copyright © 2019 小白王. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PlayViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
